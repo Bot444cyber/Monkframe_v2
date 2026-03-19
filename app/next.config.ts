60ac1024c7c6d28ab70bcce46cc1b67d5a924a01
Bot444cyber <ar3034098@gmail.com>
2026-03-19 19:36:53 +0500

    feat: add Next.js configuration file and remove root package.json.

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* This tells Next.js to build the .next folder one level up */
-  distDir: '.next',
-
   reactCompiler: true,
 
   poweredByHeader: false,
