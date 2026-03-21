778ae2d73d5dd7086403d898dea3ae5d50958beb
Bot444cyber <ar3034098@gmail.com>
2026-03-21 23:25:39 +0500

    feat: initialize backend project with package configuration, TypeScript setup, and custom Express types.

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,10 +1,19 @@
 import { users } from '../db/schema';
 import { InferSelectModel } from 'drizzle-orm';
+import * as express from 'express';
 
 type UserModel = InferSelectModel<typeof users>;
 
 declare global {
     namespace Express {
         interface User extends UserModel { }
+        interface Request {
+            user?: User;
+            logIn(user: User, done: (err: any) => void): void;
+            logIn(user: User, options: any, done: (err: any) => void): void;
+            logout(options: { keepSessionInfo?: boolean }, done: (err: any) => void): void;
+            logout(done: (err: any) => void): void;
+            isAuthenticated(): boolean;
+        }
     }
 }
